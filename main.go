5cc6cf327d3df33aa6c9709f206df6f0c858920d
Mikhail P. <mikhail.prorekhin@gmail.com>
2026-03-31 16:29:06 +1100

    a very simple implementation of cashe

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+type Cashe interface {
+	Get(string) string
+	Set(string, string)
+	Delete(string)
+}
+
+type MemoryCashe struct {
+	cashe map[string]string
+}
+
+func (m MemoryCashe) Get(key string) string {
+	return m.cashe[key]
+}
+
+func (m MemoryCashe) Set(key string, value string) {
+	m.cashe[key] = value
+}
+
+func (m MemoryCashe) Delete(key string) {
+	delete(m.cashe, key)
+}
+
+func New() Cashe {
+	return MemoryCashe{cashe: make(map[string]string)}
+}
